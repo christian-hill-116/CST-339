ed2f1de7663785918e75fcffe44ec89be9439cde
CHRISTIAN HILL <christian-hill-116@users.noreply.github.com>
2021-09-26 14:16:30 -0700

    Initial commit

---
+++ Week1/Activity1/src/main/java/com/example/demo/Activity1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from my spring boot applicaiton");
+		SpringApplication.run(Activity1Application.class, args);
+	}
+
+}
